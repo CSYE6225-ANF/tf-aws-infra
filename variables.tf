faa7173ad237c71dee6c46cf1386fde52aba02c6
angferna <angelnfernandes@gmail.com>
2024-10-08 23:22:19 -0400

    Added A3 files

---
+++ variables.tf
@@ -0,0 +1,29 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+  default     = ["10.0.0.0/20", "10.0.16.0/20", "10.0.32.0/20"]
+}
+
+variable "private_subnet_cidrs" {
+  description = "CIDR blocks for private subnets"
+  type        = list(string)
+  default     = ["10.0.128.0/20", "10.0.144.0/20", "10.0.160.0/20"]
+}
+
+variable "availability_zones" {
+  description = "Availability zones"
+  type        = list(string)
+  default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
+}
