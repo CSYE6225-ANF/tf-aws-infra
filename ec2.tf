41a451249df72e6dafa264eff1a4048706654c87
Angel N. Fernandes <102985556+angferna@users.noreply.github.com>
2024-10-23 14:07:05 -0400

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,10 +1,22 @@
 resource "aws_instance" "web_app_instance" {
-  ami                         = data.aws_ami.custom_ami.id
+  ami                         = var.custom_ami_id
   instance_type               = var.instance_type
   key_name                    = var.key_pair_name
   vpc_security_group_ids      = [aws_security_group.application_sg.id]
   subnet_id                   = aws_subnet.public_subnets[0].id
   associate_public_ip_address = true
+
+  user_data = <<-EOF
+              #!/bin/bash
+              # Create .env file in the application directory
+              touch /home/csye6225/.env
+              echo "NODE_ENV=production" >> /home/csye6225/.env
+              echo "DB_HOST=$(echo ${aws_db_instance.csye6225_rds_instance.endpoint} | cut -d ':' -f 1)" >> /home/csye6225/.env
+              echo "DB_PORT=5432" >> /home/csye6225/.env
+              echo "DB_USER=${var.db_username}" >> /home/csye6225/.env
+              echo "DB_PASSWORD=${var.db_password}" >> /home/csye6225/.env
+              echo "DB_NAME=csye6225" >> /home/csye6225/.env
+              EOF
 
   root_block_device {
     volume_size           = 25
