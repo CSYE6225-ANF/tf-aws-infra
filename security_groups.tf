3039eb74e35e8aa27da6d71ea5288cba02dab04e
Angel N. Fernandes <102985556+angferna@users.noreply.github.com>
2024-11-27 19:04:32 -0500

    Update security_groups.tf

--- security_groups.tf
+++ security_groups.tf
@@ -4,14 +4,14 @@ resource "aws_security_group" "load_balancer_sg" {
   name   = "load-balancer-sg"
 
   # Allow incoming HTTP traffic from anywhere
-  ingress {
-    description      = "Allow HTTP traffic from anywhere"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
+  # ingress {
+  #   description      = "Allow HTTP traffic from anywhere"
+  #   from_port        = 80
+  #   to_port          = 80
+  #   protocol         = "tcp"
+  #   cidr_blocks      = ["0.0.0.0/0"]
+  #   ipv6_cidr_blocks = ["::/0"]
+  # }
 
   # Allow incoming HTTPS traffic from anywhere
   ingress {
@@ -41,13 +41,13 @@ resource "aws_security_group" "application_sg" {
   vpc_id = aws_vpc.csye6225_vpc.id
   name   = "application-sg"
 
-  ingress {
-    description = "Allow SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    # cidr_blocks = ["0.0.0.0/0"]
-  }
+  # ingress {
+  #   description = "Allow SSH"
+  #   from_port   = 22
+  #   to_port     = 22
+  #   protocol    = "tcp"
+  #   # cidr_blocks = ["0.0.0.0/0"]
+  # }
 
   ingress {
     description     = "Allow Application Port"
